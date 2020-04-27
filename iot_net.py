3b3091b8f12e0b93bca4e325898f0a937c4e2484
wanyi <wanyi@zjrealtech.com>
2020-04-27 11:28:22 +0800

    iot_net update

--- iot_net.py
+++ iot_net.py
@@ -33,9 +33,6 @@ from scp import SCPClient
 
 class CustomClient:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    ssh_client = None
-    scp_client = None
-    ssh_springboard_client = None
 
     def __init__(self, target_ip, target_port, target_username, target_password,
                  springboard_ip='192.168.20.201', springboard_port=22,springboard_username='root',
@@ -66,13 +63,13 @@ class CustomClient:
                 ('192.168.20.201', 22),
                 ssh_username="root",
                 ssh_password='123456',
-                remote_bind_address=('192.168.32.222', 22),
+                remote_bind_address=(self.target_ip, self.target_port),
                 local_bind_address=('0.0.0.0', 18882),
         ) as tunnel:
             client = paramiko.SSHClient()
             client.load_system_host_keys()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect('127.0.0.1', 18882, username='root', password='123456')
+            client.connect('127.0.0.1', 18882, username=self.target_username, password=self.target_password)
             scp = SCPClient(client.get_transport(), socket_timeout=5.0)
             scp.put(self.get_package_path(package_name), '/home/root/idi', recursive=True)
             print('FINISH!')

048b7627e0a01f5cb531ac04227211986d6a4dbe
wanyi <wanyi@zjrealtech.com>
2020-04-29 22:56:31 +0800

    8皇后问题

--- stack.py
+++ stack.py
@@ -17,5 +17,28 @@ def isMatched(s: str) -> bool:
     return True
 
 
+def n_queen(nums: int):
+    res = []
+
+    def isValid(exit_seq: str, index: int) -> bool:
+        exit_len = len(exit_seq)
+        for i in range(exit_len):
+            if abs(index - int(exit_seq[i])) in (0, exit_len - i):
+                return True
+        return False
+
+    def backup(exit_seq: str, select_list: list):
+        if len(exit_seq) == nums:
+            res.append(exit_seq)
+            return
+        else:
+            for i in select_list:
+                tmp_res = isValid(exit_seq, i)
+                if tmp_res is False:
+                    backup(exit_seq+str(i), select_list)
+    backup('', [i for i in range(1,9)])
+    return res
+
+
 if __name__ == '__main__':
-    print(isMatched('[{(})]'))
+    print(len(n_queen(8)))

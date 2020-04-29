d737a40eb8dbdd1bb6d1e26528ab2c26191495cb
wanyi <wanyi@zjrealtech.com>
2020-04-30 00:09:27 +0800

    8皇后问题

--- stack.py
+++ stack.py
@@ -1,4 +1,3 @@
-
 def isMatched(s: str) -> bool:
     def isExited(p: str) -> str:
         if p == '}': return '{'
@@ -17,7 +16,22 @@ def isMatched(s: str) -> bool:
     return True
 
 
-def n_queen(nums: int):
+def getAllCol(n: int) -> list:
+    res = []
+
+    def backTracks(exit_seq: str, select_list: list):
+        if len(exit_seq) == n:
+            res.append(exit_seq)
+            return
+        else:
+            for i in select_list:
+                if exit_seq.find(str(i)) == -1:
+                    backTracks(exit_seq + str(i), select_list)
+    backTracks('', [i for i in range(1, n+1)])
+    return res
+
+
+def n_queen(nums: int) -> int:
     res = []
 
     def isValid(exit_seq: str, index: int) -> bool:
@@ -27,7 +41,7 @@ def n_queen(nums: int):
                 return True
         return False
 
-    def backup(exit_seq: str, select_list: list):
+    def backTracks(exit_seq: str, select_list: list):
         if len(exit_seq) == nums:
             res.append(exit_seq)
             return
@@ -35,10 +49,11 @@ def n_queen(nums: int):
             for i in select_list:
                 tmp_res = isValid(exit_seq, i)
                 if tmp_res is False:
-                    backup(exit_seq+str(i), select_list)
-    backup('', [i for i in range(1,9)])
-    return res
+                    backTracks(exit_seq+str(i), select_list)
+
+    backTracks('', [i for i in range(1, nums+1)])
+    return len(res)
 
 
 if __name__ == '__main__':
-    print(len(n_queen(8)))
+    print(n_queen(8))
